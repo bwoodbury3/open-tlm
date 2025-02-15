d91b0e15bea4877a8f134b9b2852b3beb6e47ac9
bwoodbury3 <brianj.woodbury@gmail.com>
2025-02-15 15:30:11 -0800

    Rename the internal canvas objects; factor out the zoom method

--- public/graph.js
+++ public/graph.js
@@ -2,6 +2,13 @@ import { ColorPicker } from "/public/color.js";
 
 const DATA_ENDPOINT = "/api/data/";
 
+/*
+ * TODO:
+ *      - Multiple axes
+ *      - Smarter axes labels (1, 2, 3, ... instead of 0.89, 1.95, 3.01, ...)
+ *      - Show the value on hover
+ */
+
 /**
  * Class which tracks the state of a zoom.
  */
@@ -72,14 +79,14 @@ export class Graph {
     /**
      * Constructor.
      *
-     * @param {Element} canvas The graph canvas layer.
-     * @param {Element} interact The interactive canvas layer.
+     * @param {Element} graph_canvas The graph canvas layer.
+     * @param {Element} interact_canvas The interactive canvas layer.
      * @param {Element} legend The legend element.
      * @param {Date} start The initial start time range.
      * @param {Date} end The initial end time range.
      * @param {Array<String>} dataset_ids The dataset IDs.
      */
-    constructor(canvas, interact, legend, start, end, dataset_ids) {
+    constructor(graph_canvas, interact_canvas, legend, start, end, dataset_ids) {
         this.start = start.getTime();
         this.end = end.getTime();
         this.datasets = {};
@@ -90,18 +97,18 @@ export class Graph {
         /*
          * Initialize the canvas.
          */
-        this.canvas = canvas;
-        this.canvas.width = this.canvas.offsetWidth;
-        this.canvas.height = this.canvas.offsetHeight;
-        this.ctx = this.canvas.getContext("2d");
+        this.graph_layer = graph_canvas;
+        this.graph_layer.width = this.graph_layer.offsetWidth;
+        this.graph_layer.height = this.graph_layer.offsetHeight;
+        this.graph_ctx = this.graph_layer.getContext("2d");
 
         /*
          * Initialize the interactive layer.
          */
-        this.interact = interact;
-        this.interact.width = this.interact.offsetWidth;
-        this.interact.height = this.interact.offsetHeight;
-        this.interact_ctx = this.interact.getContext("2d");
+        this.interact_layer = interact_canvas;
+        this.interact_layer.width = this.interact_layer.offsetWidth;
+        this.interact_layer.height = this.interact_layer.offsetHeight;
+        this.interact_ctx = this.interact_layer.getContext("2d");
 
         /*
          * Initialize the legend.
@@ -113,17 +120,19 @@ export class Graph {
          * Initialize the zoom tool.
          */
         this.zoomer = new Zoomer();
-        this.interact.addEventListener("mousedown", event => this._zoom_start(event));
-        this.interact.addEventListener("mouseup", event => this._zoom_end(event));
-        this.interact.addEventListener("mouseleave", event => this._zoom_cancel());
-        this.interact.addEventListener("mousemove", event => this._zoom_move(event));
-        this.interact.addEventListener("wheel", event => this._zoom_mousewheel(event));
+        this.interact_layer.addEventListener("mousedown", event => this._zoom_start(event));
+        this.interact_layer.addEventListener("mouseup", event => this._zoom_end(event));
+        this.interact_layer.addEventListener("mouseleave", event => this._zoom_cancel());
+        this.interact_layer.addEventListener("mousemove", event => this._zoom_move(event));
+        this.interact_layer.addEventListener("wheel", event => this._zoom_mousewheel(event));
 
         addEventListener("resize", event => this._on_resize());
 
         for (const dataset_id of dataset_ids) {
             this._fetch(dataset_id);
         }
+
+        this._graph_layer();
     }
 
     /**
@@ -183,10 +192,10 @@ export class Graph {
      * Resize and redraw the canvas.
      */
     _on_resize() {
-        this.canvas.width = this.canvas.offsetWidth;
-        this.canvas.height = this.canvas.offsetHeight;
-        this.interact.width = this.interact.offsetWidth;
-        this.interact.height = this.interact.offsetHeight;
+        this.graph_layer.width = this.graph_layer.offsetWidth;
+        this.graph_layer.height = this.graph_layer.offsetHeight;
+        this.interact_layer.width = this.interact_layer.offsetWidth;
+        this.interact_layer.height = this.interact_layer.offsetHeight;
         this._graph_layer();
     }
 
@@ -197,7 +206,7 @@ export class Graph {
         /*
          * Prepare canvas for rendering.
          */
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.graph_ctx.clearRect(0, 0, this.graph_layer.width, this.graph_layer.height);
 
         /*
          * Calculate the y axis range.
@@ -213,6 +222,14 @@ export class Graph {
             }
         }
 
+        /*
+         * Give a sane default if there is no data.
+         */
+        if (Object.keys(this.datasets).length === 0) {
+            min_y = 0;
+            max_y = 1;
+        }
+
         const x_range = this.end - this.start;
         var y_range = max_y - min_y;
 
@@ -223,8 +240,8 @@ export class Graph {
         min_y -= y_range * .05;
         y_range = max_y - min_y;
 
-        const x_scale = this.canvas.width / x_range;
-        const y_scale = this.canvas.height / y_range;
+        const x_scale = this.graph_layer.width / x_range;
+        const y_scale = this.graph_layer.height / y_range;
 
         /*
          * Draw all of the paths.
@@ -239,7 +256,7 @@ export class Graph {
             if (!(dataset_id in this.colors)) {
                 this.colors[dataset_id] = this.color_picker.next();
             }
-            this.ctx.strokeStyle = this.colors[dataset_id];
+            this.graph_ctx.strokeStyle = this.colors[dataset_id];
 
             /*
              * Draw the initial point.
@@ -247,8 +264,8 @@ export class Graph {
             const d0 = new Date(points[0].date).getTime();
             const x0 = x_scale * (d0 - this.start);
             const y0 = y_scale * (max_y - points[0].value);
-            this.ctx.beginPath();
-            this.ctx.moveTo(x0, y0);
+            this.graph_ctx.beginPath();
+            this.graph_ctx.moveTo(x0, y0);
 
             /*
              * Draw the subsequent points.
@@ -258,14 +275,14 @@ export class Graph {
                 const x = x_scale * (d - this.start);
                 const y = y_scale * (max_y - this._get_value(point));
 
-                this.ctx.lineTo(x, y);
+                this.graph_ctx.lineTo(x, y);
             }
 
-            this.ctx.stroke();
+            this.graph_ctx.stroke();
         }
 
         /*
-         * Redraw the axis.
+         * Redraw the axes.
          */
         this._axes(min_y, max_y);
 
@@ -283,22 +300,22 @@ export class Graph {
      */
     _axes(min_y, max_y) {
         const margin_px = 10;
-        this.ctx.fillStyle = "white";
-        this.ctx.font = "15px Arial";
+        this.graph_ctx.fillStyle = "white";
+        this.graph_ctx.font = "15px Arial";
 
         /*
          * X axis (date).
          */
         const x_points = 7;
         const x_range = this.end - this.start;
-        const x_scale = this.canvas.width / x_range;
-        const x_dist_px = this.canvas.width - 2 * margin_px;
+        const x_scale = this.graph_layer.width / x_range;
+        const x_dist_px = this.graph_layer.width - 2 * margin_px;
         const x_step_px = x_dist_px / (x_points - 1);
         for (let i = 0; i < x_points - 1; ++i) {
             const x_px = margin_px + i * x_step_px;
             const x_val = this.start + x_px / x_scale;
             const datestr = new Date(x_val).toISOString();
-            this.ctx.fillText(`${datestr}`, x_px, this.canvas.height - margin_px);
+            this.graph_ctx.fillText(`${datestr}`, x_px, this.graph_layer.height - margin_px);
         }
 
         /*
@@ -306,14 +323,14 @@ export class Graph {
          */
         const y_points = 10;
         const y_range = max_y - min_y;
-        const y_scale = this.canvas.height / y_range;
-        const y_dist_px = this.canvas.height - 2 * margin_px;
+        const y_scale = this.graph_layer.height / y_range;
+        const y_dist_px = this.graph_layer.height - 2 * margin_px;
         const y_step_px = y_dist_px / (y_points - 1);
         for (let i = 1; i < y_points - 1; ++i) {
             const y_px = margin_px + i * y_step_px;
             const y_val = max_y - y_px / y_scale;
             const valstr = y_val.toFixed(2);
-            this.ctx.fillText(`${valstr}`, margin_px, y_px);
+            this.graph_ctx.fillText(`${valstr}`, margin_px, y_px);
         }
     }
 
@@ -357,16 +374,15 @@ export class Graph {
         /*
          * Prepare canvas for rendering.
          */
-        const height = this.interact.height;
-        const width = this.interact.width;
+        const height = this.interact_layer.height;
+        const width = this.interact_layer.width;
         this.interact_ctx.clearRect(0, 0, width, height);
 
         /*
-         * If we're zooming, grayed out boxes around the areas _not_ affected by the
-         * zoom.
+         * If we're zooming, gray out the area outside the zoom range.
          */
         if (this.zoomer.zooming && this.zoomer.should_zoom()) {
-            this.interact_ctx.fillStyle = 'rgba(225,225,225,0.2)';
+            this.interact_ctx.fillStyle = 'rgba(225,225,225,0.1)';
 
             if (this.zoomer.axis === "x") {
                 const min_x = Math.min(this.zoomer.x0, this.zoomer.x1);
@@ -427,7 +443,7 @@ export class Graph {
          */
         if (this.zoomer.axis === "x") {
             const x_range = this.end - this.start;
-            const x_scale = this.interact.width / x_range;
+            const x_scale = this.interact_layer.width / x_range;
             this.end = this.start + this.zoomer.x1 / x_scale;
             this.start = this.start + this.zoomer.x0 / x_scale;
         } else {
@@ -436,6 +452,10 @@ export class Graph {
              */
         }
 
+        /*
+         * Draw the graph immediately for instant gratificaiton.
+         */
+        this._graph_layer();
         this._refresh();
     }
 
@@ -466,6 +486,8 @@ export class Graph {
      *
      * The desired behavior in a mousewheel zoom is that the x coordinate at the cursor
      * remains constant, while the range zooms out by some factor.
+     *
+     * @param {*} event
      */
     _zoom_mousewheel(event) {
         if (this.zoomer.axis === "y") {
@@ -475,35 +497,37 @@ export class Graph {
             return;
         }
 
-        /*
-         * One tick of the wheel is a 5% zoom.
-         */
-        const zoom_factor = .2;
-        const x_range_old = this.end - this.start;
-        const x_scale_old = this.interact.width / x_range_old;
+        const zoom_factor = event.deltaY > 0 ? 0.2 : -0.2;
         const mouse_x = event.offsetX;
-        const mouse_date = this.start + mouse_x / x_scale_old;
+        this._zoom_once_x(zoom_factor, mouse_x);
+    }
+
+    /**
+     * Zoom once. Positive out, negative in.
+     *
+     * @param {Number} zoom_factor The percent to zoom.
+     * @param {Number} midpoint_x The centerpoint of the zoom in the canvas frame.
+     */
+    _zoom_once_x(zoom_factor, midpoint_x) {
+        /*
+         * Calculate the old/new zoom ranges.
+         */
+        const x_range_old = this.end - this.start;
+        const x_scale_old = this.interact_layer.width / x_range_old;
+        const x_range_desired = x_range_old * (1 + zoom_factor);
+        const x_scale_desired = this.interact_layer.width / x_range_desired;
 
         /*
-         * Zoom out.
+         * Calculate the new start/end points relative to the zoom midpoint.
          */
-        if (event.deltaY > 0) {
-            const x_range_desired = x_range_old * (1 + zoom_factor);
-            const x_scale_desired = this.interact.width / x_range_desired;
-            this.start = mouse_date -  mouse_x / x_scale_desired;
-            this.end = this.start + x_range_desired;
-        }
+        const mouse_date = this.start + midpoint_x / x_scale_old;
+        this.start = mouse_date -  midpoint_x / x_scale_desired;
+        this.end = this.start + x_range_desired;
 
         /*
-         * Zoom in.
+         * Draw the graph immediately while polling for higher fidelity data.
          */
-        if (event.deltaY < 0) {
-            const x_range_desired = x_range_old * (1 - zoom_factor);
-            const x_scale_desired = this.interact.width / x_range_desired;
-            this.start = mouse_date -  mouse_x / x_scale_desired;
-            this.end = this.start + x_range_desired;
-        }
-
+        this._graph_layer();
         this._refresh();
     }
 }
